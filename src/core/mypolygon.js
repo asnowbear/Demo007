e6239c6f5c0165adabeb95a43293ff7165bcee1a
zybear <1106408264@qq.com>
2017-12-14 22:37:18 +0800

    update

--- src/core/mypolygon.js
+++ src/core/mypolygon.js
@@ -3,12 +3,48 @@
 function MyPolygon () {
   this.coords = []
   this.feature = {}
-  
+  this.tag = ''
   
   
 }
 
 
-MyPolygon.prototype.draw = function () {
-  
+MyPolygon.prototype.setPosition = function (values) {
+  this.coords = values
+}
+
+MyPolygon.prototype.draw = function (context) {
+  var coords = this.coords
+  ctx.save()
+
+  ctx.beginPath()
+
+  // const coordinates = renderOpt.coordinates
+  ctx.moveTo(coordinates[0][0],coordinates[0][1])
+
+  for (var k = 0, kk = coords.length ; k < kk ; k ++){
+    let cd = coordinates[k]
+    ctx.lineTo(cd[0],cd[1])
+  }
+
+  if (renderOpt.fillStyle) {
+    ctx.fillStyle = renderOpt.fillStyle
+    ctx.fill()
+  }
+
+  ctx.closePath()
+
+  const borderStyle = renderOpt.borderStyle
+  if (borderStyle) {
+    ctx.strokeStyle = colorToString(borderStyle.color,borderStyle.alpha)
+    ctx.lineWidth = borderStyle.width
+
+    if (borderStyle.lineDash) {
+      ctx.setLineDash(borderStyle.lineDash)
+    }
+
+    ctx.stroke()
+  }
+
+  ctx.restore()
 }
--- src/core/paint.js
+++ src/core/paint.js
@@ -1,11 +1,13 @@
 
 
 function Paint (config) {
-  
+  this.map = null
   this._mouseDownPoint = null
   this._type = config.type || 'polygon'
   this._tempPolygon = null
   this._tempPositions = null
+  this._tempLinePositions = null
+  this._killCoordinates = null
   
   
 }
@@ -31,17 +33,69 @@ Paint.prototype.handleMouseDown = function (evt) {
 }
 
 Paint.prototype.handleMouseMove = function (evt) {
-  
   var point = [evt.mapX, evt.mapY]
   
   if (this._tempPolygon === null) {
     this._tempPolygon = new MyPolygon()
     this._tempPolygon.setPosition([point])
+  } else {
+    this.updateDrawing(point)
   }
 }
 
+Paint.prototype.updateDrawing = function (point) {
+
+}
+
 Paint.prototype.handleMouseUp = function (evt) {
-  
+  var downPt = this._mouseDownPoint
+  var clickPt = [evt.mapX, evt.mapY]
+
+  var dx = downPt[0] - clickPt[0],
+      dy = downPt[1] - clickPt[1]
+
+  var dist = dx * dx + dy * dy
+
+  if (dist <= 36) {
+    if (!this._killCoordinates) {
+      this._doDrawing(evt)
+    }
+    // else if (this._atFinish(evt)) {
+    //   if (this._finishCondition(evt)) {
+    //     this._finishDrawing()
+    //   }
+    // }
+    else {
+      this._continuesToDrawing(evt)
+    }
+  }
+}
+
+Paint.prototype._continuesToDrawing = function(evt) {
+  var pt = [evt.mapX, evt.mapY]
+
+  var coordinates = this._tempPositions
+  coordinates.push(pt.slice())
+
+  this._killCoordinate = coordinates[0]
+
+  this._tempPolygon.setPosition(this._tempPositions)
+  this.map.refresh()
+}
+
+Paint.prototype._doDrawing = function(evt) {
+  var pt = [evt.mapX, evt.mapY]
+  this._killCoordinates = pt
+
+  this._tempPositions = [pt.slice(), pt.slice()]
+
+  var polygon = new MyPolygon()
+  polygon.tag = 'temp'
+  polygon.setPosition(this._tempPositions)
+  this._tempPolygon = polygon
+
+  this.map.datasource.push(this._tempPolygon)
+  this.map.refresh()
 }
 
 Paint.prototype.setMap = function(map) {
@@ -49,8 +103,12 @@ Paint.prototype.setMap = function(map) {
 }
 
 Paint.prototype.draw = function(map) {
+  var datasource = map.datasource
+  var map = this.map
 
-
+  datasource.forEach(function(geo){
+    geo.draw(map.context)
+  })
 }
 
 
